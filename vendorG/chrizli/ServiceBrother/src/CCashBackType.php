af60e6c3a79843b2bb5f61ce2a45b48f13603ef5
ChrisLi <Christian.Listl@googlemail.com>
2018-05-15 21:52:21 +0200

    upd ServiceBrother

--- vendorG/chrizli/ServiceBrother/src/CCashBackType.php
+++ vendorG/chrizli/ServiceBrother/src/CCashBackType.php
@@ -1,22 +1,25 @@
 <?php
 //20180509, ListlChr, CashBackTypes holds basic data for each CashBackType, use Factory to create object
-class CCashBackType {
+
+class   CCashBackType 
+extends CBase
+{
     private $a=array('sId'=>'', 'sNameShort'=>'', 'sPayoutTypeId'=>'');
     
-    public function __construct() {}
+    public  function    __construct() {}
     
-    public function fnSet(
-        $_sId,
-        $_sNameShort,
-        $_sPayoutTypeId
-    ){
-        $a['sId']           = strToUpper($_sId);
-        $a['sNameShort']    = strToUpper($_sNameShort);
-        $a['sPayoutTypeId'] = strtoUpper($_sPayoutTypeId);
+    public  function    fnSet(
+            string $_sId,
+            string $_sNameShort,
+            string $_sPayoutTypeId
+    ): void {
+            $this->a['sId']           = strToUpper($_sId);
+            $this->a['sNameShort']    = strToUpper($_sNameShort);
+            $this->a['sPayoutTypeId'] = strtoUpper($_sPayoutTypeId);
     }
     
-    public function fnGet():array {
-        return $a;
+    public  function    fnGet():array {
+            return $this->a;
     }
 }
 
--- vendorG/chrizli/ServiceBrother/src/CSalesOrgFactory.php
+++ vendorG/chrizli/ServiceBrother/src/CSalesOrgFactory.php
@@ -1,49 +1,52 @@
 <?php
 // 20180509, ListlChr, creates array of CSalesOrg based on give array of sId
 // e.g. fnSet(('BIG','BAT')) will create CSalesOrg objects for BIG and BAT
-class CSalesOrgFactory {
-    private $aSoId  = array();
-    private $aSo    = array();
+class       CSalesOrgFactory 
+extends     CBase
+{
+    private 
+            $aSoId  = array(),
+            $aSo    = array();
     
-    public function __construct() {
-        $this->fnInit();
+    public  function    __construct() {
+            $this->fnInit();
     }
     
-    private function fnInit() {
-        $this->fnIdInit();
+    private function    fnInit() {
+            $this->fnIdInit();
     }
     
-    private function fnIdInit() {
-        $this->aSoId = array('BIG','BAT','BUK');
+    private function    fnIdInit() {
+            $this->aSoId = array('BIG','BAT','BUK');
     }
     
-    public function fnSet($_a) {
-        forEach($_a as $sSo) {
-            $o = new CSalesOrg();
-            switch ($sSo) {
-                case 'BIG':
-                    $o->fnSet('BIG','BIG');
-                    break;
-                case 'BAT':
-                    $o->fnSet('BAT','BAT');
-                    break;
-                case 'BUK':
-                    $o->fnSet('BUK','BUK');
-                    break;
-                default:
-                    $o->fnSet('NA','NA');
-                    //throw \new error()
+    public  function    fnSet(array $_a): void {
+            forEach($_a as $sSo) {
+                $o = new CSalesOrg();
+                switch ($sSo) {
+                    case 'BIG':
+                        $o->fnSet('BIG','BIG');
+                        break;
+                    case 'BAT':
+                        $o->fnSet('BAT','BAT');
+                        break;
+                    case 'BUK':
+                        $o->fnSet('BUK','BUK');
+                        break;
+                    default:
+                        $o->fnSet('NA','NA');
+                        //throw \new error()
+                }
+                array_push($this->aSo, $o);
             }
-            array_push($this->aSo, $o);
-        }
     }
     
-    public function fnGet() {
-        return $this->aSo;
+    public  function    fnGet() {
+            return $this->aSo;
     }
     
-    public function fnIdValid($_sId) {
-        return (array_search($_sId, $this->aSoId))?true:false;
+    public  function    fnIdValid(string $_sId) {
+            return (array_search($_sId, $this->aSoId))?true:false;
     }
 }
 

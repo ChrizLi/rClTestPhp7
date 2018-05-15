77a07de36c8346ca10861155898630337f7ccf89
EU\ListlChr <listlc@brother.de>
2018-05-15 15:30:58 +0200

    add autoLoad er v3

--- vendorG/chrizli/basicPhp/sandpit/nameSpaceV3/autoLoad03.php
+++ vendorG/chrizli/basicPhp/sandpit/nameSpaceV3/autoLoad03.php
@@ -1,10 +1,13 @@
 <html>
 <head>
 <?php
+    error_reporting(E_STRICT);
     function fnMain() {
-        include $_SERVER['DOCUMENT_ROOT'].'/vendorG/chrizli/basicPhp/sandpit/nameSpaceV3/autoLoad03Sub.php';
+        include $_SERVER['DOCUMENT_ROOT'].'/vendorG/chrizli/basicPhp/src/autoLoad03Sub.php';
         fnAutoLoadSetup();
         
+        //$oUser = new chrizli\basicPhp\CUser();
+        //$oUser->fnSet(array('sId'=>'listlchr', 'sAdminPath'=>'\\BigW10N61014\tempCB$'));
         // samples
         $oC1 = new chrizli\basicPhp\C1();
         $oC1->fnPrint('xy');

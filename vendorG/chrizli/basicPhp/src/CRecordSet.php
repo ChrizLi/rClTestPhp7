6f53eaaff7fb6d9d1670ef61c0acaa974be9870d
EU\ListlChr <listlc@brother.de>
2018-05-18 08:27:06 +0200

    add CRecordSet

--- vendorG/chrizli/basicPhp/src/CRecordSet.php
+++ vendorG/chrizli/basicPhp/src/CRecordSet.php
@@ -10,34 +10,43 @@ extends     CBase
             $aKey;
             
     public  function    __construct(array $_aKey) {
+            // plain array, non associated
             $this->aKey = $_aKey;
     }
     
     public  function    fnSet(array $_aRecordSet): void {
-            forEach($aKey as $sKey) {
-                if (array_key_exists($sKey, $_aRecordSet)!===false) {
-                $this->aKey[$sKey]=$_aRecordSet[$sKey];
+            forEach($this->aKey as $sKey) {
+                if (array_key_exists($sKey, $_aRecordSet)!==false) {
+                    $this->aRecordSet[$sKey]=$_aRecordSet[$sKey];
+                }
             }
     }
     
-    public  function    fnKeyExits(string $_sKey): bool {
+    public  function    fnKeyExists(string $_sKey): bool {
             return (array_search($_sKey, $this->aKey)===false)? false: true;
     }
     
-    public  function    fnGet(string $_sKey=null) {
+    public  function    fnGet(string $_sKey=null, bool $_bErrorThrow=null) {
+            $_bErrorThrow = ($_bErrorThrow==null)? true: $_bErrorThrow;
             if ($_sKey==null) {
                 return  $this->aRecordSet;
             }   else {
-                if ($this->fnExists($_sKey)) {
+                if ($this->fnKeyExists($_sKey)) {
                     return $this->aRecordSet[$_sKey];
+                }   else {
+                    if ($_bErrorThrow) {
+                        $this->fnErrorThrow('ArgIsNotValid');
+                    }   else {
+                        return null;
+                    }
                 }
             }
     }
     
     public  function    fnValueExists(array $_aKeyValue) {
             $sKey = key($_aKeyValue);
-            if ($this->fnKeyExists($sKey) {
-                if ($aRecordSet[$sKey]==$_aKeyValue[$sKey]) {
+            if ($this->fnKeyExists($sKey)) {
+                if ($this->aRecordSet[$sKey]==$_aKeyValue[$sKey]) {
                     return  true;
                 }   else {
                     return  false;
--- vendorG/chrizli/basicPhp/test/CRecordSetTest.php
+++ vendorG/chrizli/basicPhp/test/CRecordSetTest.php
@@ -1,33 +1,74 @@
-    public  function    testFnSet(): void {
+<?php
+declare(strict_types=1);
+require_once('_cfg.php');
+require_once (__ROOT__.'/vendor/autoload.php');
+require_once (__ROOT__.'/vendorG/chrizLi/basicPhp/src/CBase.php');
+require_once (__ROOT__.'/vendorG/chrizLi/basicPhp/src/CRecordSet.php');
+
+use PHPUnit\Framework\TestCase;
+use chrizli\basicPhp as cPhp;
+
+final class CRecordSetTest extends TestCase
+{
+    
+    public  function    testFnSetGet(): void {
         $aKey   = array('s1', 's2');
-        $oCut   = new CRecordSet($aKey);
+        $oCut   = new cPhp\CRecordSet($aKey);
+        $aValue = array('s1'=>'v1', 's2'=>'v2');
+        $oCut->fnSet($aValue);
+        $this->assertEquals(
+            'v1',
+            $oCut->fnGet('s1'),
+            'default'
+        );
+        $this->assertEquals(
+            $aValue,
+            $oCut->fnGet(),
+            'full'
+        );
+        $this->expectException(exception::class);
+        $this->assertEquals(
+            'not',
+            $oCut->fnGet('not'),
+            'not'
+        );
+    }
+    
+    public  function    testFnSetExists(): void {
+        $aKey   = array('s1', 's2');
+        $oCut   = new cPhp\CRecordSet($aKey);
         $aValue = array('s1'=>'v1', 's2'=>'v2');
         $oCut->fnSet($aValue);
         $this->assertEquals(
             true,
-            $this->fnKeyValid('s1'),
+            $oCut->fnKeyExists('s1'),
             'keyTrue'
         );
         $this->assertEquals(
             false,
-            $this->fnKeyValid('not'),
+            $oCut->fnKeyExists('not'),
             'keyFalse'
         );
         $this->assertEquals(
             true,
-            $this->fnValueValid(array('s1'=>'v1')),
+            $oCut->fnValueExists(array('s1'=>'v1')),
             'valueTrue'
-        )
+        );
         $this->assertEquals(
             false,
-            $this->fnValueValid(array('s1'=>'v3')),
+            $oCut->fnValueExists(array('s1'=>'v3')),
             'valueFalse'
-        )
+        );
+        $this->expectException(exception::class);
         $this->assertEquals(
             false,
-            $this->fnValueValid(array('not'=>'v1')),
+            $oCut->fnValueExists(array('not'=>'v1')),
             'keyFalse2'
-        )
+        );
     }
+}
+?>
 
+
+    
     

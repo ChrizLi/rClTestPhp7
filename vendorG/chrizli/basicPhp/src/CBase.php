35e474e0263ba10fbb061e81df062e083a920c21
ChrisLi <Christian.Listl@googlemail.com>
2018-10-15 21:26:12 +0200

    move

--- vendorG/chrizli/basicPhp/src/CBase.php
+++ vendorG/chrizli/basicPhp/src/CBase.php
@@ -5,15 +5,16 @@ namespace   chrizli\basicPhp;
 class       CBase 
 {
     // baseClass for all classes
-    protected function fnErrorThrow(
-            string $_sType  = '', 
-            string $_sMsg   = ''
-            ): void {
+    protected 
+            function fnErrorThrow(
+            string  $_sType = '', 
+            string  $_sMsg  = ''
+            ):      void    {
             $sTypeOut       = '';
             $sMsgOut        = '';
             switch($_sType) {
                 case 'ArgNotValid':
-                    $sTypeOut='ArgumentNotValidException';
+                    $sTypeOut='ArgNotValidException';
                     break;
                 default:
                     $sTypeOut='MyUndefinedException';
